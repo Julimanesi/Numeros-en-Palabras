236ba59ffbf6795558aff56f1752bf0ce07d0df8
julimanesi94 <julimanesi94@gmail.com>
2022-08-29 19:27:44 -0300

    Agregar archivos de proyecto.

---
+++ Numeros en Palabras/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Numeros_en_Palabras
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.numero = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.palabra_sal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numero)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese un número:";
+            // 
+            // numero
+            // 
+            this.numero.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numero.Location = new System.Drawing.Point(212, 17);
+            this.numero.Maximum = new decimal(new int[] {
+            999999999,
+            0,
+            0,
+            0});
+            this.numero.Name = "numero";
+            this.numero.Size = new System.Drawing.Size(150, 26);
+            this.numero.TabIndex = 1;
+            this.numero.ValueChanged += new System.EventHandler(this.ingreso);
+            this.numero.Enter += new System.EventHandler(this.answer_Enter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(51, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(209, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "El número en palabras es el ";
+            // 
+            // palabra_sal
+            // 
+            this.palabra_sal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.palabra_sal.Location = new System.Drawing.Point(257, 74);
+            this.palabra_sal.Name = "palabra_sal";
+            this.palabra_sal.Size = new System.Drawing.Size(577, 43);
+            this.palabra_sal.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(855, 143);
+            this.Controls.Add(this.palabra_sal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numero);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = resources.GetString("$this.Text");
+            this.TransparencyKey = System.Drawing.Color.DarkSlateBlue;
+            ((System.ComponentModel.ISupportInitialize)(this.numero)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numero;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label palabra_sal;
+    }
+}
+
---
+++ Numeros en Palabras/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Numeros_en_Palabras
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int nro;
+        private void ingreso(object sender, EventArgs e)
+        {
+            nro =(int) numero.Value;
+            palabra_sal.Text=funcion3(nro);
+        }
+        
+        private string funcion3(int nro)
+	{   	int millonecima,nrores;
+		string conc,millones=(" millones "),millon=("un millon ");
+		millonecima=nro/1000000;
+        conc="";
+		if(millonecima>0){if(millonecima==1){conc=millon;if(nro==1000000){return conc;}}else{conc+=funcion(millonecima);conc+=millones;}}
+		nrores=nro-(millonecima*1000000);
+		conc+=funcion2(nrores);
+		return conc;
+		}
+
+        private string funcion2(int nro)
+	{	int milena,nrores;
+	    string conc,mil=(" mil ");
+        conc = "";
+		milena=nro/1000;
+		if(milena>0){if(milena==1){conc=mil;if(nro==1000){return conc;}}else{conc+=funcion(milena);conc+=mil;}} 
+		nrores=nro-(milena*1000);
+		conc+=funcion(nrores);
+		return conc;
+		}
+        /// <summary>
+        /// toma un número como parametro que es menor a 100
+        /// y devuelve el nro en forma coloquial
+        /// </summary>
+        /// <param name="nro"></param>
+        /// <returns></returns>
+        private string funcion(int nro)
+           {int unidad,decena,centena;   
+            string[] unidades={"uno","dos" ,"tres","cuatro","cinco","seis","siete","ocho","nueve"};
+            string[] decenas1={"diez","once","doce","trece","catorce","quince"};
+            string[] centenas={"cien","ciento ","doscietos ","trescientos ","cuatrocientos ","quinientos ","seiscientos ","setecientos ","ochocientos ","novecientos "};
+            string[] decenas2={"diez","veinte","treinta","cuarenta","cincuenta","sesenta","setenta","ochenta","noventa"};        
+            string  y=(" y "),veinti=("veinti");
+            string conc;
+            conc="";
+            centena=nro/100;
+            decena=(nro-(centena*100))/10;//primero separo el nro en unidades, decenas y centenas
+            unidad=nro-(centena*100+decena*10);           
+            if(centena>0){if(nro==100){conc=centenas[0];return conc;}else {conc+=centenas[centena];}}
+            switch(decena){
+                   case 0: if(unidad==0){return conc;} else{
+						  conc+=unidades[unidad-1];return conc ;}
+						  break; //  si no hay decenas muestro unidades solas   
+                   case 2: if(unidad==0){   
+									  conc+=decenas2[decena-1];
+                                      return  conc;}
+                        	 else{         // si no concateno 
+                                      conc+=veinti;
+                                      conc+=unidades[unidad-1];
+                                      return conc; }
+						break;
+				   case 3: case 4: case 5: case 6: case 7: case 8: case 9:
+                        if(unidad==0){
+									  conc+=decenas2[decena-1];
+                                      return  conc;}// si las decenas no tienen unidad  tambien las muestro solas
+                        else{         // si no concateno 
+                                      conc+=decenas2[decena-1];
+                                      conc+=y;
+                                      conc+=unidades[unidad-1];
+                                      return conc;        }
+                   break;
+                   default: if(unidad<6) {
+								conc+=decenas1[unidad];
+								return conc;}
+                            else { conc+=decenas2[decena-1];
+								conc+=y;
+								conc+=unidades[unidad-1];
+								return conc;
+							break;     
+                                 }
+            } 
+			                   
+           }
+
+        private void answer_Enter(object sender, EventArgs e)
+        {
+            // Select the whole answer in the NumericUpDown control.
+            NumericUpDown answerBox = sender as NumericUpDown;
+
+            if (answerBox != null)
+            {
+                int lengthOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lengthOfAnswer);
+            }
+
+        }
+
+      
+
+        
+      
+    }
+}
